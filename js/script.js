6ac794e478243ea5690267c1939062f081264558
Higman <s15t201@stu.kagawa-u.ac.jp>
2020-03-06 13:17:06 +0900

    add/add contents about license and readme

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+/*
+Copyright (c) 2019 Christopher Antonellis & @ctrl-freaks
+Code released under the MIT license
+https://opensource.org/licenses/mit-license.php
+*/
+
 new Freezeframe('.freezeframe-gif', {
   overlay: true
 });
